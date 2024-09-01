cf2f6384724f7086e9465c49040e90754330980e
lamini_abdel <abdellahlamini2001@gmail.com>
2024-09-01 13:43:15 +0100

    the demo of the chat App

---
+++ src/main/java/org/example/simplechatapp/Chat/MessageType.java
@@ -0,0 +1,8 @@
+package org.example.simplechatapp.Chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+
+    LEAVE
+}
---
+++ src/test/java/org/example/simplechatapp/SimpleChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.simplechatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
